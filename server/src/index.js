ac46f14c6da151d83052169a6df0e492a49beffa
Gab <gabionian7@gmail.com>
2026-03-02 16:06:14 +0800

    vercel

--- server/src/index.js
+++ server/src/index.js
@@ -5,17 +5,17 @@ require('dotenv').config();
 
 const app = express();
 app.set('trust proxy', 1);
-const frontendUrl = 'https://xientech.vercel.app';
+const frontendUrl = 'https://ocrfrontend-six.vercel.app';
 
 // 1. Use the middleware (remove the manual app.options block below this) this is for prod
-// app.use(cors({
-//   origin: frontendUrl,
-//   credentials: true,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
+app.use(cors({
+  origin: frontendUrl,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
-app.use(cors()); // local
+// app.use(cors()); // local
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
